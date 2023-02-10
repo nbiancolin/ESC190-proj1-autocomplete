074ab4869181ee622c0c6bfed13eaffd82e7ff15
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2023-02-10 13:51:41 -0500

    still broken but want to back it up

--- autocomplete.c
+++ autocomplete.c
@@ -9,20 +9,25 @@ void read_in_terms(struct term **terms, int *pnterms, char *filename){
     FILE *file;
     file = fopen(filename, "r");
 
-    *terms = malloc((int)sizeof(char) * *pnterms); //Allocates enough memory for the file.
+    //*terms = malloc((int)sizeof(char) * *pnterms); //Allocates enough memory for the file.
+    *terms; //= pointer to all the words in memory.
+
 
     char ch;
-    char *buffer = '\0';
+    char *buffer = '\0'; //Purpose of the buffer (in my mind) was to store the word until a tab is found, then store the pointer to the buffer in the term struct.
 
-    while((ch = fgetc(file)) != EOF){ //This *should* work... no idea if it actually does haha.
+    while((ch = fgetc(file)) != EOF){ //Finds the number of terms in the file.
         if(ch == '\t'){
-            *pnterms += 1;
-            **terms->term = buffer; //doesnt work.. idky why
-            *buffer = '\0';
+            *pnterms += 1; //I found out how to count the terms in this file, but don't know how to store them.
+            //**terms->term = buffer; //should store the pointer to the buffer in the term struct.
+            //*buffer = '\0';
         } else {
-            buffer = strcat(buffer, (char *) ch);
+            //buffer = strcat(buffer, (char *) ch);
         }
+
     }
+
+    *terms = malloc((int)sizeof(char) * *pnterms); //Allocates enough memory for the file.
 
     fclose(file);
 
